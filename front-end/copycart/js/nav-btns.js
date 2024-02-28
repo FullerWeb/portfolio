607b0f4012aee7ddd4efc8fda2fdf177ae879704
FullerWeb <160547678+FullerWeb@users.noreply.github.com>
2024-02-27 19:22:53 -0500

    Add files via upload

---
+++ front-end/copycart/js/nav-btns.js
@@ -0,0 +1,7 @@
+document.getElementById("back-nav").addEventListener("click", function () {
+  window.history.back();
+});
+
+document.getElementById("forward-nav").addEventListener("click", function () {
+  window.history.forward();
+});
