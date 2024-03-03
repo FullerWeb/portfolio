2825eae7e069765ef8e35497199155956a4ebf7c
FullerWeb <160547678+FullerWeb@users.noreply.github.com>
2024-03-02 22:55:56 -0500

    Add files via upload

---
+++ front-end/waggingChronicle/js/breedsCarousel.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll('.carousel .slide');
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  showSlide(currentIndex);
+}
+
+document.querySelector('.prev').addEventListener('click', prevSlide);
+document.querySelector('.next').addEventListener('click', nextSlide);
--- front-end/waggingChronicle/js/script.js
+++ front-end/waggingChronicle/js/script.js
@@ -1,1 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var overlay = document.getElementById('overlay');
+  var disclaimer = document.getElementById('disclaimer');
+  var closeBtn = document.getElementById('closeBtn');
+
+  // Initially hide the disclaimer
+  disclaimer.style.bottom = '-100%';
+
+  overlay.style.display = 'block';
+  disclaimer.style.display = 'block';
+
+  setTimeout(function () {
+    overlay.style.opacity = '1';
+    // Animate the disclaimer to rise from the bottom
+    disclaimer.style.bottom = '0';
+    // Re-enable scrolling
+    document.documentElement.style.overflowY = 'scroll';
+  }, 100);
+
+  closeBtn.addEventListener('click', function () {
+    overlay.style.opacity = '0';
+    // Animate the disclaimer to move back down
+    disclaimer.style.bottom = '-100%';
+    // Set overflow-y to scroll initially to avoid shifting
+    document.documentElement.style.overflowY = 'scroll';
+    setTimeout(function () {
+      overlay.style.display = 'none';
+      disclaimer.style.display = 'none';
+      // Re-enable scrolling
+      document.body.style.overflow = 'auto';
+    }, 500); // Wait for the same duration as the transition (0.5s)
+  });
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.menu-toggle').classList.toggle('active');
+    document.querySelector('.nav-links').classList.toggle('active');
+  });
+});
+
 
